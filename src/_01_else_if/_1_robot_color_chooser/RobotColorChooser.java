2057fe97e26af5545373895123b4cc0df216eca5
Charles <Charles@appledeair.lan>
2020-09-04 17:02:15 -0700

    nice

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -1,6 +1,10 @@
 
 package _01_else_if._1_robot_color_chooser;
-
+/*
+ *    Copyright (c) The League of Amazing Programmers 2013-2020
+ *    Level 0
+ */
+import org.jointheleague.graphical.robot.Robot;
 import javax.swing.JOptionPane;
 import java.awt.Color;
 
@@ -8,7 +12,36 @@ public class RobotColorChooser {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-	    //2. Make the robot draw a shape (this will take more than one line of code)		
+		Robot gg = new Robot();
+	    //2. Make the robot draw a shape (this will take more than one line of code)	
+		while (true) {
+		gg.clear();
+			gg.setPenWidth(10);
+		String c= JOptionPane.showInputDialog("What color do you want?");
+		if(c.equalsIgnoreCase("Blue") ) {
+			gg.setPenColor(Color.blue);
+		}
+		if(c.equalsIgnoreCase("Red") ) {
+			gg.setPenColor(Color.red);
+		}
+		if(c.equalsIgnoreCase("Yellow") ) {
+			gg.setPenColor(Color.yellow);
+		}
+		else {
+			gg.setRandomPenColor();
+		}
+		gg.setSpeed(100);
+		gg.penDown();
+		gg.setAngle(90);
+		gg.move(300);
+		gg.setAngle(180);
+		gg.move(300);
+		gg.setAngle(-90);
+		gg.move(300);
+		gg.setAngle(0);
+		gg.move(300);
+		}
+		
 		//3. Set the pen width to 10
 		//4. Ask the user what color pen they would like the robot to draw with
 		//5. Use an if/else statement to set the pen color that the user requested
